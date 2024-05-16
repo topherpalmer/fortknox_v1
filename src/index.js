e2857d91d3171a54dd28a875d93770aba542a9d8
topherpalmer <topherpalmer@users.noreply.github.com>
2024-05-16 09:56:56 -0500

    well well another another cleanup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { RetoolApp } from 'retool-react';
 import reportWebVitals from './reportWebVitals';
 
 Amplify.configure(config);
