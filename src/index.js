9fc8ac70678cd6408b3a541c8c617b6c0443457b
topherpalmer <topherpalmer@users.noreply.github.com>
2024-05-16 10:06:26 -0500

    well well well another another update to cleanup

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-Amplify.configure(config);
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
