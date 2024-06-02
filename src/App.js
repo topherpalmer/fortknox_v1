3d1992899c7b72d469e70bce21c359de755212f6
topherpalmer <topherpalmer@gmail.com>
2024-06-01 21:46:50 -0500

    Update App.js removed VStudio "stashed" comments

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
 import {
@@ -131,7 +107,6 @@ function App({ signOut }) {
         <Button onClick={signOut}>Sign Out</Button>
       </View>
     );
->>>>>>> Stashed changes
 }
 
 export default App;
